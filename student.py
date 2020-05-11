40467a773a72df55449411b92cf0aa28b0ba7002
Igor <igorlongorocha@gmail.com>
2020-05-11 00:03:32 -0300

    Started working on NN layers, not working yet. Dataloader now splits data into train and test sets.

--- student.py
+++ student.py
@@ -3,14 +3,24 @@ import numpy as np
 import pandas as pd
 import os
 from torch.utils import data
+from torch.utils.data import dataloader as DataLoader
 import torch
 from torchvision import transforms
 from natsort import natsorted, ns
 import cv2
 from PIL import Image
 import matplotlib.pyplot as plt
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+
 
 dataset_path = "/home/igor/mlprojects/Csgo-NeuralNetwork/output/"
+#train_split and test_split 0.1 > x > 0.9 and must add up to 1
+train_split = 0.7
+test_split = 0.3
+num_epochs = 2
+batch_size = 10
 # NOTE: possibly remove in next version??
 # video_folder_name = "CSGOraw1"
 # label_index = 12
@@ -89,6 +99,30 @@ class CsgoPersonDataset(data.Dataset):
 
         return sample
 
+
+class Net(nn.Module):
+    def __init__(self):
+        super(Net, self).__init__()
+        self.conv1 = nn.Conv2d(3, 6, 5)
+        self.pool = nn.MaxPool2d(2, 2)
+        self.conv2 = nn.Conv2d(6, 16, 5)
+        self.fc1 = nn.Linear(16 * 5 * 5, 120)
+        self.fc2 = nn.Linear(120, 84)
+        self.fc3 = nn.Linear(84, 10)
+
+    def forward(self, x):
+        x = self.pool(F.relu(self.conv1(x)))
+        x = self.pool(F.relu(self.conv2(x)))
+        x = x.view(-1, 16 * 5 * 5)
+        x = F.relu(self.fc1(x))
+        x = F.relu(self.fc2(x))
+        x = self.fc3(x)
+        return x
+
+
+net = Net()
+
+
 transform = transforms.Compose([
     transforms.Resize([320, 180]),
     transforms.ToTensor()
@@ -96,6 +130,47 @@ transform = transforms.Compose([
 
 dataset = CsgoPersonDataset(dataset_path, transform)
 
-dataloader = torch.utils.data.DataLoader(dataset, batch_size=5,
-                                        shuffle=True, num_workers=4)
+dataset_len = len(dataset)
+print(dataset_len)
 
+train_split = int(np.floor(dataset_len * train_split))
+test_split = int(np.floor(dataset_len * test_split))
+while train_split + test_split != dataset_len:
+    train_split += 1
+train_set, test_set = torch.utils.data.random_split(\
+    dataset, [train_split, test_split])
+
+train_loader = DataLoader(dataset=train_set, batch_size=batch_size, transform=transform, shuffle=True, num_workers=4)
+test_loader = DataLoader(dataset=test_set, batch_size=batch_size, transform=transform, shuffle=True, num_workers=4)
+
+classes = ('background', 'enemy')
+
+criterion = nn.CrossEntropyLoss()
+optimizer = optim.SGD(net.parameters(), lr=0.001, momentum=0.9)
+
+
+
+for epoch in range(num_epochs):  # loop over the dataset multiple times
+
+    running_loss = 0.0
+    for i, data in enumerate(trainloader, 0):
+        # get the inputs; data is a list of [inputs, labels]
+        inputs, labels = data
+
+        # zero the parameter gradients
+        optimizer.zero_grad()
+
+        # forward + backward + optimize
+        outputs = net(inputs)
+        loss = criterion(outputs, labels)
+        loss.backward()
+        optimizer.step()
+
+        # print statistics
+        running_loss += loss.item()
+        if i % 2000 == 1999:    # print every 2000 mini-batches
+            print('[%d, %5d] loss: %.3f' %
+                  (epoch + 1, i + 1, running_loss / 2000))
+            running_loss = 0.0
+
+print('Finished Training')
